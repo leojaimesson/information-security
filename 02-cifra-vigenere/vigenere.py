ceb4d48697caf5e5094dc18c694408944bf3ea87
leojaimesson <leo.jaimesson.dev@gmail.com>
2018-03-13 19:53:34 -0300

    encrypt e decrypt implementado

--- 02-cifra-vigenere/vigenere.py
+++ 02-cifra-vigenere/vigenere.py
@@ -1,11 +1,28 @@
-ALPHABET = "ABCDEFGHIJKLMNOPQRTSUVWXYZ"
+CHAR_A = ord('A')
+SIZE_ALPHABET = 26
 
-def createMatrix():
-    mat = []
-    for i in range(26):
-        mat.append([])
-        for j in range(26):
-            mat[i].append(ALPHABET[(i + j) % 26])
-    return mat
+def normalizeKey(message, key):
+    keyNormalized = ""
+    for char in message:
+        keyNormalized += key[0]
+        key = key[1:] + key[0]
+    return keyNormalized
 
-print createMatrix()
+def encrypt(message, key):
+    result = ""
+    for index in range(len(message)):
+        result += chr((ord(message[index]) + ord(key[index]) - (CHAR_A * 2)) % SIZE_ALPHABET + CHAR_A)
+    return result
+
+def decrypt(message, key):
+    result = ""
+    for index in range(len(message)):
+        result += chr(((ord(message[index]) - ord(key[index])) + SIZE_ALPHABET) % SIZE_ALPHABET + CHAR_A)
+    return result
+
+message = raw_input("message:")
+key = raw_input("key:")
+
+keyNormalized = normalizeKey(message, key)
+
+print decrypt(message, keyNormalized)
