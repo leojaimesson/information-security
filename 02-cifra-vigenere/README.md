529cf1d7d9a8446cf8f8b4db3bf6626131fd4def
leojaimesson <leo.jaimesson.dev@gmail.com>
2018-03-16 15:04:42 -0300

    Implementação da cifra e vigenere concluida.

---
+++ 02-cifra-vigenere/README.md
@@ -0,0 +1,11 @@
+# Cifra de Vigenere
+
+## Relatório
+
+Eu resolvi a o exercício proposto individualmente. Para implementação dessa cifra optei por utilizar a linguagem python devido afinidade que eu possuo com a mesma. Eu fiz tudo o que foi pedido no trabalho. No total da atividade gastei cerca de 1 hora e meia para concluir. Para obter possíveis palavras que poderiam ser a chave que gerou a mensagem criptografada eu usei um dicionário com algumas palavras da língua portuguesa. Consegui descriptografar a mensagem passada como desafio.
+
+## Executar Projeto
+
+Execute `python vigenere.py [path message] [path wordlist]` no diretório raiz do projeto.
+
+Exemplo: `python vigenere.py message.txt wordlist.txt`
--- 02-cifra-vigenere/vigenere.py
+++ 02-cifra-vigenere/vigenere.py
@@ -1,28 +1,57 @@
-CHAR_A = ord('A')
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import unicodedata
+import re
+
+UNICODE_A = ord('A')
+UNICODE_Z = ord('Z')
 SIZE_ALPHABET = 26
+PATH_DECRYPTED_LOG = "tmp/decrypted.log"
+READ = "r"
+WRITE = "w"
 
 def normalizeKey(message, key):
     keyNormalized = ""
     for char in message:
-        keyNormalized += key[0]
-        key = key[1:] + key[0]
+        if(char == " "):
+            keyNormalized += " "
+        elif (char == ","):
+            keyNormalized += ","
+        else:
+            keyNormalized += key[0]
+            key = key[1:] + key[0]
     return keyNormalized
+
+def normalizeText(text, codif='utf-8'):
+    return unicodedata.normalize('NFKD', text.decode(codif)).encode('ASCII','ignore').upper()
 
 def encrypt(message, key):
     result = ""
     for index in range(len(message)):
-        result += chr((ord(message[index]) + ord(key[index]) - (CHAR_A * 2)) % SIZE_ALPHABET + CHAR_A)
+        if ord(message[index]) >= UNICODE_A and ord(message[index]) <= UNICODE_Z: 
+            result += chr((ord(message[index]) + ord(key[index]) - (UNICODE_A * 2)) % SIZE_ALPHABET + UNICODE_A)
+        else:
+            result += message[index]
     return result
 
 def decrypt(message, key):
     result = ""
     for index in range(len(message)):
-        result += chr(((ord(message[index]) - ord(key[index])) + SIZE_ALPHABET) % SIZE_ALPHABET + CHAR_A)
+        if ord(message[index]) >= UNICODE_A and ord(message[index]) <= UNICODE_Z: 
+            result += chr(((ord(message[index]) - ord(key[index])) + SIZE_ALPHABET) % SIZE_ALPHABET + UNICODE_A)
+        else:
+            result += message[index]
     return result
 
-message = raw_input("message:")
-key = raw_input("key:")
-
-keyNormalized = normalizeKey(message, key)
-
-print decrypt(message, keyNormalized)
+def main():
+    params = sys.argv[1:]
+    message = open(params[0], READ).readlines()
+    keys = open(params[1], READ)
+    result = open(PATH_DECRYPTED_LOG, WRITE)
+    for l in keys:
+        result.write(l + decrypt(normalizeText(message[0]), normalizeKey(normalizeText(message[0]), normalizeText(l.replace('\n', '').replace('-','')))))
+        result.write("\n\n")
+if __name__ == '__main__':
+    main()
